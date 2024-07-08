40c751373c1c20f6afbb139c6561e9d5462c4ef8
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-07-08 09:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+        document.getElementById("openModal").onclick = function() {
+            document.getElementById("modal").style.display = "block";
+        };
+
+        document.getElementById("closeModal").onclick = function() {
+            document.getElementById("modal").style.display = "none";
+        };
+
+        window.onclick = function(event) {
+            if (event.target == document.getElementById("modal")) {
+                document.getElementById("modal").style.display = "none";
+            }
+        };
